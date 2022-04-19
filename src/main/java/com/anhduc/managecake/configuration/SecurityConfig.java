c2fd9dcd10101026abada9a41af4ffa8924e1f2e
anhduc1603 <saotrucanhduc@gmail.com>
2022-04-19 15:04:47 +0700

    19/04/03pm

--- src/main/java/com/anhduc/managecake/configuration/SecurityConfig.java
+++ src/main/java/com/anhduc/managecake/configuration/SecurityConfig.java
@@ -1,35 +1,43 @@
-package com.anhduc.managecake.configuration;
-
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/","/shop/**","/forgotpassword","/register","/h2-console/**")
-                .permitAll()
-                .antMatchers("/admin/**")
-                .hasRole("ADMIN")
-                .anyRequest()
-                .authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .failureUrl("login?error=true")
-                .defaultSuccessUrl("/")
-                .usernameParameter("email")
-                .passwordParameter("password")
-                .and()
-                .oauth2Login()
-                .loginPage("/login")
-                .successHandler(googleOauth2SuccessHandler)
-    }
-}
+//package com.anhduc.managecake.configuration;
+//
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/","/shop/**","/forgotpassword","/register","/h2-console/**")
+//                .permitAll()
+//                .antMatchers("/admin/**")
+//                .hasRole("ADMIN")
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .formLogin()
+//                .loginPage("/login")
+//                .permitAll()
+//                .failureUrl("login?error=true")
+//                .defaultSuccessUrl("/")
+//                .usernameParameter("email")
+//                .passwordParameter("password")
+//                .and()
+//                .oauth2Login()
+//                .loginPage("/login")
+//                .successHandler(googleOauth2SuccessHandler)
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                .logoutSuccessUrl("/login")
+//                .invalidateHttpSession(true)
+//                .deleteCookies("")
+//                .
+//    }
+//}
